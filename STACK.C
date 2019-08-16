03348b0dd2a4e2617bcaffc2ea9d8db693266ee3
sahanal2603 <53811265+sahanal2603@users.noreply.github.com>
2019-08-16 09:55:57 -0400

    Add files via upload

--- STACK.C
+++ STACK.C
@@ -1,68 +1,115 @@
 #include<stdio.h>
 #include<conio.h>
-#define max_size 10
-int item[max_size];
-int top=-1;
-int x;
-void push(int x)
+#include<string.h>
+int max=50;
+void push();
+void peek();
+void display();
+void pop();
+int isempty();
+int isfull();
+struct student
 {
- printf("enter a value for x");
- scanf("%d",&x);
- if (isfull())
-   printf("error:stack full");
+ char usn[10];
+ char name[25];
+ char mail[30];
+};
+struct student s1[50];
+int top=-1;
+int main()
+{
+ int choice;
+ clrscr();
+ do
+ {
+  printf("\n ****STACK MENU****");
+  printf("\n 1.push\n2.pop\n3.peek\n4.display\n5.exit");
+  printf("\n Enter your choice");
+  scanf("%d",&choice);
+  switch(choice)
+  {
+   case 1:push();
+	  break;
+   case 2:pop();
+	  break;
+   case 3:peek();
+	  break;
+   case 4:display();
+	  break;
+   case 5:exit(0);
+	  break;
+   default:exit(0);
+  }
+ }while(choice>-1);
+ getch();
+ return 0;
+}
+void push()
+{
+ if(isfull())
+  printf("\n STACK OVERFLOW");
  else
-   x=item[++top];
-   printf("%d",x);
+ {
+  top=top+1;
+  printf("\n enter usn:");
+  scanf("%s",&s1[top].usn);
+  printf("\n enter name:");
+  scanf("%s",&s1[top].name);
+  printf("\n enter email:");
+  scanf("%s",&s1[top].mail);
+ }
 }
 void pop()
 {
+ int x;
  if(isempty())
-   printf("error:stack empty");
+  printf("\n STACK UNDERFLOW");
  else
-   top--;
-}void display()
-{
- int i;
- printf("stack");
- for(i=0;i<=max_size;i++)
  {
-  printf("%d",i);
-  printf("\n");
+  x=top;
+  printf("\n popped student ");
+  printf("\n usn=%s",s1[x].usn);
+  printf("\n name=%s",s1[x].name);
+  printf("\n mail=%s",s1[x].mail);
+  top=top-1;
  }
 }
-int isfull()
+void display()
 {
- if(top==max_size)
-   return 1;
+ int i;
+ if(isempty())
+  printf("\n STACK UNDERFLOW");
  else
-   return 0;
+ {
+  printf("\n STUDENT DETAILS");
+  for(i=top;i>=0;i--)
+  {
+   printf("\n usn=%s",s1[i].usn);
+   printf("\n name=%s",s1[i].name);
+   printf("\n mail=%s\n",s1[i].mail);
+  }
+ }
+}
+void peek()
+{
+ if(isempty())
+  printf("\n STACK UNDERFLOW");
+ else
+  printf("\n usn=%s",s1[top].usn);
+  printf("\n name=%s",s1[top].name);
+  printf("\n mail=%s",s1[top].mail);
 }
 int isempty()
 {
  if(top==-1)
-   return 1;
+  return 1;
  else
-   return 0;
+  return 0;
 }
-int main()
+int isfull()
 {
- int choice;
- printf("enter your choice");
- scanf("%d",&choice);
- switch(choice)
- {
- case 1:
- push(x);
- break;
- case 2:
- pop();
- break;
- case 3:
- display();
- break;
- default:
- exit();
-}
-getch();
-return 0;
+ if(top==max-1)
+  return 1;
+ else
+  return 0;
 }
