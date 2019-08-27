0f5bac0212232aacc0ad487bde078cf030ff6ff3
sahanal2603 <53811265+sahanal2603@users.noreply.github.com>
2019-08-27 08:21:48 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,69 +1,63 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define MAX 4
-
-void enqueue();
-void dequeue();
-void display();
-
-int main()
+#define size 5
+void enqueue(int q[], int*r)
 {
- int ch, *front=-1,*rear=-1,*a[MAX];
- printf("\n STACK MENU \n 1)insert 2)delete 3)display 4)exit");
- while(ch!=4)
- {
-  printf("\n Enter choice");
-  scanf("%d",&ch);
-  switch(ch)
-  {
-   case 1:enqueue(int rear,int front,int a[MAX]);
-   break;
-   case 2:dequeue(int rear,int front,int a[MAX]);
-   break;
-   case 3:display(int rear,int front,int a[MAX]);
-   break;
-   case 4:exit(0);
-   break;
-   default:printf("\n Invalid choice");
-  }
- }
-
- return 0;
-}
-void enqueue()
+int item;
+if(*r==size-1)
+printf("Queue is full\n");
+else
 {
- int x;
- if(*rear==MAX-1)
-  printf("\n Overflow");
- else
- {
-  if(*front==-1)
-   *front=0;
-  printf("\n Enter element to insert");
-  scanf("%d",&x);
-  *rear=*rear+1;
-  *a[rear]=x;
- }
+printf("Enter the value to be entered\n");
+scanf("%d", &item);
+(*r)++;
+q[*r]=item;
 }
-void dequeue(int *rear,int *front,int *a[MAX])
+}
+void dequeue(int q[], int*r, int *f)
 {
- if(*front==-1||*front>*rear)
-  printf("\n Underflow");
- else
- {
-  printf("\n Deleted element is %d",*a[front]);
-  front=front+1;
- }
-}
-void display(int *rear,int *front, int *a[MAX])
+if(*f>*r)
+printf("Queue is empty\n");
+else
 {
- int i;
- if(*front==-1)
-  printf("\n empty");
- else
- {
-  for(i=*front;i<=*rear;i++)
-   printf("\n %d",a[i]);
- }
+printf("Item deleted is: %d\n", q[*f]);
+(*f)++;
 }
- 
+}
+void display(int q[], int*r, int*f)
+{
+int i;
+if(*f>*r)
+printf("Queue is empty:\n");
+for(i=*f; i<=*r; i++)
+{
+printf("%d\n", q[i]);
+}
+}
+void main()
+{
+int q[20];
+int r=-1;
+int f=0;
+int a;
+while(1)
+{
+printf("\n1.Insert \n");
+printf("2.Delete\n");
+printf("3.Display\n");
+printf("4.Exit\n");
+scanf("%d", &a);
+switch(a)
+{
+case 1: enqueue(q, &r);
+	break;
+case 2: dequeue(q,&r,&f);
+	break;
+case 3: display(q, &r,&f);
+	break;
+case 4: exit(0);
+	break;
+default: printf("INVALID CHOICE\n");
+}
+}
+}
